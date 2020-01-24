a2eb0686640f9c419ef65aed52f7198e337e9ea0
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2020-01-24 16:39:44 +0100

    Display embryo and rng fix

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,11 @@
 module Main exposing (Model, Msg, init, main, subscriptions, update, view)
 
 import Browser
+import Css exposing (..)
 import Html.Styled as Html exposing (..)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing (..)
+import Minesweeper as MS
 import TicTacToe as TTT
 
 
@@ -15,33 +19,107 @@ main =
         }
 
 
+type GameModel
+    = TicTacToe TTT.Model
+    | Minesweeper MS.Model
+
+
 type alias Model =
-    { tictactoe : TTT.Model
+    { gameModel : GameModel
     }
 
 
 init : flags -> ( Model, Cmd Msg )
-init flags =
-    ( { tictactoe = TTT.init }, Cmd.none )
+init _ =
+    ( { gameModel = TicTacToe TTT.init
+      }
+    , Cmd.none
+    )
+
+
+type ExtMsg
+    = TicTacToeMsg TTT.Msg
+    | MinesweeperMsg MS.Msg
 
 
 type Msg
-    = TicTacToeMsg TTT.Msg
+    = ExternalMessage ExtMsg
+    | GameChanged GameModel
+
+
+dispatch :
+    ExtMsg
+    -> Model
+    -> a
+    -> (TTT.Msg -> TTT.Model -> a)
+    -> (MS.Msg -> MS.Model -> a)
+    -> a
+dispatch msg model default ttt ms =
+    case msg of
+        TicTacToeMsg msg1 ->
+            case model.gameModel of
+                TicTacToe model1 ->
+                    ttt msg1 model1
+
+                _ ->
+                    default
+
+        MinesweeperMsg msg1 ->
+            case model.gameModel of
+                Minesweeper model1 ->
+                    ms msg1 model1
+
+                _ ->
+                    default
+
+
+mapUpdate :
+    (msg -> model -> ( model, Cmd msg ))
+    -> (msg -> ExtMsg)
+    -> (model -> GameModel)
+    -> Model
+    -> msg
+    -> model
+    -> ( Model, Cmd Msg )
+mapUpdate upd toMsg toModel model1 msg model =
+    let
+        ( m, c ) =
+            upd msg model
+    in
+    ( { model1 | gameModel = toModel m }, Cmd.map (toMsg >> ExternalMessage) c )
+
+
+updateTTT :
+    Model
+    -> TTT.Msg
+    -> TTT.Model
+    -> ( Model, Cmd Msg )
+updateTTT =
+    mapUpdate TTT.update TicTacToeMsg TicTacToe
+
+
+updateMS :
+    Model
+    -> MS.Msg
+    -> MS.Model
+    -> ( Model, Cmd Msg )
+updateMS =
+    mapUpdate MS.update MinesweeperMsg Minesweeper
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        TicTacToeMsg tttmsg ->
-            let
-                (m, c) =
-                    TTT.update tttmsg model.tictactoe
-            in
-            ( { model | tictactoe = m }, Cmd.map TicTacToeMsg c )
+    case msg of 
+        ExternalMessage msg1 ->
+            dispatch msg1 model ( model, Cmd.none ) (updateTTT model) (updateMS model)
+
+        GameChanged newGame ->
+            ( {model | gameModel = newGame }, Cmd.none )
+        
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions _ =
     Sub.none
 
 
@@ -49,8 +127,48 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "Games!"
     , body =
-        [ toUnstyled <|
-            div []
-                [ text "TicTacToe", Html.map (\msg -> TicTacToeMsg msg) <| TTT.view model.tictactoe ]
+        [ tabed model |> toUnstyled
         ]
     }
+
+
+tabsFor : Model -> Html Msg
+tabsFor model =
+    let
+        games =
+            [ ( "Minesweeper", Minesweeper MS.init )
+            , ( "TicTacToe", TicTacToe TTT.init )
+            ]
+
+        style =
+            [ displayFlex ]
+
+        tabStyle =
+            []
+
+        mkDiv ( name, initModel ) =
+            div [ css tabStyle, onClick (GameChanged initModel) ] [ text name ]
+    in
+    games
+        |> List.map mkDiv
+        |> div [ css style ]
+
+
+tabed : Model -> Html Msg
+tabed model =
+    div []
+        [ tabsFor model
+        , gameView model.gameModel
+        ]
+
+
+gameView : GameModel -> Html Msg
+gameView m =
+    case m of
+        TicTacToe model ->
+            div []
+                [ text "TicTacToe", Html.map (TicTacToeMsg >> ExternalMessage) (TTT.view model) ]
+
+        Minesweeper model ->
+            div []
+                [ text "Minesweeper", Html.map (MinesweeperMsg >> ExternalMessage) (MS.view model) ]
--- src/Minesweeper.elm
+++ src/Minesweeper.elm
@@ -2,14 +2,16 @@ module Minesweeper exposing (Model, Msg, init, update, view)
 
 import Array exposing (Array)
 import Dict exposing (Dict)
-import Html.Styled exposing (Html, Attribute, button, div, text, input)
-import Html.Styled.Attributes
-import Html.Styled.Attributes exposing (css, type_)
-import Html.Styled.Events exposing (onClick)
 import Html.Events.Extra
+import Html.Styled exposing (Attribute, Html, button, div, input, text)
+import Html.Styled.Attributes exposing (css, type_, value)
+import Html.Styled.Events exposing (onClick)
 import Random
 
-onChange = Html.Styled.Attributes.fromUnstyled << Html.Events.Extra.onChange
+
+onChange =
+    Html.Styled.Attributes.fromUnstyled << Html.Events.Extra.onChange
+
 
 type Cell
     = Mine
@@ -47,13 +49,18 @@ type Msg
     | MinChanged Int
     | CountChanged Int
 
-number : List (Attribute msg) -> Int -> Html msg
-number attr content = input (type_ "number" :: attr) [text <| String.fromInt content]
+
+number : Int -> List (Attribute msg) -> List (Html msg) -> Html msg
+number val attr content =
+    input (type_ "number" :: (value <| String.fromInt val) :: attr) content
+
 
 tryMsg : (Int -> Msg) -> Int -> String -> Msg
-tryMsg m i = String.toInt
-         >> Maybe.withDefault i
-         >> m
+tryMsg m i =
+    String.toInt
+        >> Maybe.withDefault i
+        >> m
+
 
 view : Model -> Html Msg
 view model =
@@ -66,26 +73,32 @@ view model =
                         List.map String.fromInt <|
                             Array.toList model.list
             )
-        , number [onChange (tryMsg MinChanged model.max)] model.max
-        , number [onChange (tryMsg MaxChanged model.max)] model.max
-        , number [onChange (tryMsg CountChanged model.max)] model.max
+        , number model.min [ onChange (tryMsg MinChanged model.min) ] []
+        , number model.max [ onChange (tryMsg MaxChanged model.max) ] []
+        , number model.count [ onChange (tryMsg CountChanged model.count) ] []
         ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
-    case message of 
-        NewListGenerated array -> ({model | list = array}, Cmd.none)
+    case message of
+        NewListGenerated array ->
+            ( { model | list = array }, Cmd.none )
 
-        RequestedNewList -> (model, Random.generate NewListGenerated (reservoirSample model.count (Array.initialize model.max (\i -> i + model.min))))
+        RequestedNewList ->
+            ( model, Random.generate NewListGenerated (reservoirSample model.count (Array.initialize (model.max - model.min + 1) (\i -> i + model.min))) )
 
-        MaxChanged m -> ({model | max = m}, Cmd.none)
+        MaxChanged m ->
+            ( { model | max = m }, Cmd.none )
 
-        MinChanged m -> ({model | min = m}, Cmd.none)
+        MinChanged m ->
+            ( { model | min = m }, Cmd.none )
 
-        CountChanged c -> ({model | count = c}, Cmd.none) 
+        CountChanged c ->
+            ( { model | count = c }, Cmd.none )
 
-        _ -> (model, Cmd.none)
+        _ ->
+            ( model, Cmd.none )
 
 
 init : Model
@@ -136,32 +149,48 @@ reservoirSample n source =
         next i x =
             randomF |> Random.map (\r -> 1 + i + floor (logBase e r / logBase e (1 - x)))
 
+        randomI : Random.Generator Int
+        randomI =
+            Random.int 0 (n - 1)
+
+        repl i list k =
+            Array.get i source
+                |> Maybe.map (\e -> Array.set k e list)
+                |> Maybe.withDefault list
+
         loop : Int -> Array a -> Float -> Random.Generator (Array a)
         loop i list x =
             if i >= max then
                 Random.constant list
 
             else
-                let
-                    randomI : Random.Generator Int
-                    randomI =
-                        Random.int 0 (n - 1)
-
-                    repl k =
-                        Array.get i source
-                            |> Maybe.map (\e -> Array.set k e list)
-                            |> Maybe.withDefault list
-                in
                 randomI
                     |> Random.andThen
                         (\k ->
                             next i x
-                                |> Random.andThen (\j -> loop j (repl k) x)
+                                |> Random.andThen
+                                    (\j ->
+                                        w
+                                            |> Random.andThen (\y -> loop j (repl i list k) (x * y))
+                                    )
                         )
     in
-    w
-        |> Random.andThen
-            (\x ->
-                next (n - 1) x
-                    |> Random.andThen (\j -> loop j result x)
-            )
+    if n <= 0 || max == 0 then
+        Random.constant Array.empty
+
+    else if n == 1 then
+        Random.int 0 (max - 1)
+            |> Random.map
+                (\i ->
+                    Array.get i source
+                        |> Maybe.map (\x -> Array.fromList [x])
+                        |> Maybe.withDefault Array.empty
+                )
+
+    else
+        w
+            |> Random.andThen
+                (\x ->
+                    next (n - 1) x
+                        |> Random.andThen (\j -> loop j result x)
+                )
