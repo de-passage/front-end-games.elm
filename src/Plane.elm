940668298715cf0a513862de672aecb6b2c08a8b
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2020-01-31 10:37:05 +0100

    Adds a bunch of point manipulation function

--- src/Plane.elm
+++ src/Plane.elm
@@ -4,29 +4,40 @@ module Plane exposing
     , Width(..)
     , X(..)
     , Y(..)
+    , Point
     , at
     , clampAt
+    , clampSetAt
     , clampToCoordinates
+    , clampUpdateAt
     , defaultInitialize
+    , down
     , fromCoordinates
+    , fromPoint
     , get
-    , initialize
-    , toCoordinates
     , getFromCoordinates
-    , wrapAt
-    , wrapToCoordinates
+    , height
+    , initialize
+    , left
+    , pointToPlane
+    , right
     , set
     , setAt
-    , clampSetAt
-    , wrapSetAt
+    , toArray
+    , toCoordinates
+    , toList
+    , up
     , update
     , updateAt
-    , clampUpdateAt
-    , wrapUpdateAt
-    , height
     , width
-    , fromPoint
-    , pointToPlane
+    , wrapAt
+    , wrapDown
+    , wrapLeft
+    , wrapRight
+    , wrapSetAt
+    , wrapToCoordinates
+    , wrapUp
+    , wrapUpdateAt
     )
 
 import Array exposing (Array)
@@ -55,8 +66,10 @@ type Height
 type Plane a
     = Plane (Array a) Height Width a
 
-type Point a 
+
+type Point a
     = Point Coordinates (Plane a)
+
 
 initialize : Height -> Width -> (Coordinates -> a) -> Plane a
 initialize (Height h) (Width w) f =
@@ -96,6 +109,7 @@ fromCoordinates : Coordinates -> Plane a -> ( X, Y )
 fromCoordinates (C i) (Plane _ _ (Width w) _) =
     ( X (i // w), Y (modBy w i) )
 
+
 toPoint : X -> Y -> Plane a -> Maybe (Point a)
 toPoint (X x) (Y y) (Plane p (Height h) (Width w) d) =
     if x < 0 || y < 0 || x >= h || y >= h then
@@ -104,17 +118,26 @@ toPoint (X x) (Y y) (Plane p (Height h) (Width w) d) =
     else
         Just (Point (C (x * w + y)) (Plane p (Height h) (Width w) d))
 
-fromPoint : Point a -> (Coordinates, Plane a)
-fromPoint (Point c p) = (c, p)
+
+fromPoint : Point a -> ( Coordinates, Plane a )
+fromPoint (Point c p) =
+    ( c, p )
+
 
 pointToPlane : Point a -> Plane a
-pointToPlane (Point _ p) = p
+pointToPlane (Point _ p) =
+    p
+
 
 pointToCoordinates : Point a -> Coordinates
-pointToCoordinates (Point c _) = c
+pointToCoordinates (Point c _) =
+    c
+
 
 clampToCoordinates : X -> Y -> Plane a -> Coordinates
-clampToCoordinates x y p = pointToCoordinates (clampToPoint x y p)
+clampToCoordinates x y p =
+    pointToCoordinates (clampToPoint x y p)
+
 
 clampToPoint : X -> Y -> Plane a -> Point a
 clampToPoint (X x) (Y y) (Plane p (Height h) (Width w) d) =
@@ -127,8 +150,11 @@ clampToPoint (X x) (Y y) (Plane p (Height h) (Width w) d) =
     in
     Point (C (x1 * w + y1)) (Plane p (Height h) (Width w) d)
 
+
 wrapToCoordinates : X -> Y -> Plane a -> Coordinates
-wrapToCoordinates x y p = pointToCoordinates (wrapToPoint x y p)
+wrapToCoordinates x y p =
+    pointToCoordinates (wrapToPoint x y p)
+
 
 wrapToPoint : X -> Y -> Plane a -> Point a
 wrapToPoint (X x) (Y y) (Plane p (Height h) (Width w) d) =
@@ -161,8 +187,8 @@ getFromCoordinates (C c) (Plane a _ _ _) =
     Array.get c a
 
 
-get : Point a -> a
-get (Point (C c) (Plane a _ _ d)) =
+at : Point a -> a
+at (Point (C c) (Plane a _ _ d)) =
     case Array.get c a of
         Just r ->
             r
@@ -171,19 +197,19 @@ get (Point (C c) (Plane a _ _ d)) =
             d
 
 
-at : X -> Y -> Plane a -> Maybe a
-at (X x) (Y y) (Plane p _ (Width w) _) =
+get : X -> Y -> Plane a -> Maybe a
+get (X x) (Y y) (Plane p _ (Width w) _) =
     Array.get (x * w + y) p
 
 
 clampAt : X -> Y -> Plane a -> a
 clampAt x y p =
-    get (clampToPoint x y p)
+    at (clampToPoint x y p)
 
 
 wrapAt : X -> Y -> Plane a -> a
 wrapAt x y p =
-    get (wrapToPoint x y p)
+    at (wrapToPoint x y p)
 
 
 set : Coordinates -> a -> Plane a -> Plane a
@@ -201,20 +227,27 @@ wrapSetAt : X -> Y -> a -> Plane a -> Plane a
 wrapSetAt x y a p =
     set (wrapToCoordinates x y p) a p
 
+
 clampSetAt : X -> Y -> a -> Plane a -> Plane a
 clampSetAt x y a p =
     set (clampToCoordinates x y p) a p
 
+
 update : (a -> a) -> Point a -> Plane a
-update f (Point (C c) (Plane p h w d)) = 
-    case Array.get c p of 
-        Nothing -> Plane p h w d
-        Just a -> Plane (Array.set c (f a) p) h w d
+update f (Point (C c) (Plane p h w d)) =
+    case Array.get c p of
+        Nothing ->
+            Plane p h w d
+
+        Just a ->
+            Plane (Array.set c (f a) p) h w d
+
 
 updateAt : X -> Y -> (a -> a) -> Plane a -> Maybe (Plane a)
 updateAt x y f p =
     toPoint x y p
         |> Maybe.map (\c -> update f c)
+
 
 clampUpdateAt : X -> Y -> (a -> a) -> Plane a -> Plane a
 clampUpdateAt x y f p =
@@ -224,9 +257,60 @@ clampUpdateAt x y f p =
 wrapUpdateAt : X -> Y -> (a -> a) -> Plane a -> Plane a
 wrapUpdateAt x y f p =
     update f (wrapToPoint x y p)
-       
+
+
 height : Plane a -> Height
-height (Plane _ h _ _) = h
-       
+height (Plane _ h _ _) =
+    h
+
+
 width : Plane a -> Width
-width (Plane _ _ w _) = w
+width (Plane _ _ w _) =
+    w
+
+
+left : X -> Y -> Plane a -> Maybe (Point a)
+left x (Y y) =
+    toPoint x (Y (y - 1))
+
+
+right : X -> Y -> Plane a -> Maybe (Point a)
+right x (Y y) =
+    toPoint x (Y (y + 1))
+
+
+up : X -> Y -> Plane a -> Maybe (Point a)
+up (X x) y =
+    toPoint (X (x - 1)) y
+
+
+down : X -> Y -> Plane a -> Maybe (Point a)
+down (X x) y =
+    toPoint (X (x + 1)) y
+
+
+wrapLeft : X -> Y -> Plane a -> Point a
+wrapLeft x (Y y) =
+    wrapToPoint x (Y (y - 1))
+
+
+wrapRight : X -> Y -> Plane a -> Point a
+wrapRight x (Y y) =
+    wrapToPoint x (Y (y + 1))
+
+
+wrapUp : X -> Y -> Plane a -> Point a
+wrapUp (X x) y =
+    wrapToPoint (X (x - 1)) y
+
+
+wrapDown : X -> Y -> Plane a -> Point a
+wrapDown (X x) y =
+    wrapToPoint (X (x + 1)) y
+
+toArray : Plane a -> Array (Point a)
+toArray (Plane b h w d) = 
+    Array.indexedMap (\i _ -> Point (C i) (Plane b h w d)) b
+
+toList : Plane a -> List (Point a)
+toList = toArray >> Array.toList
--- src/Snake.elm
+++ src/Snake.elm
@@ -1,19 +1,40 @@
-module Snake exposing (..)
+module Snake exposing (Cell(..), Model, Msg, init, update, view)
 
-import Html.Styled exposing (..)
-import Html.Styled.Events exposing (..)
 import Css exposing (..)
-import Plane exposing (..)
 import Function exposing (..)
+import Html.Styled exposing (Attribute, Html, text)
+import Html.Styled.Events exposing (..)
+import Plane exposing (Plane, Point)
 
-type alias Msg = ()
-type alias Model = ()
 
-init : (Model, Cmd Msg)
-init = ((), Cmd.none)
+type alias Msg =
+    ()
+
+
+type Cell
+    = Target
+    | Wall
+    | Empty
+
+type Direction = Left | Right | Up | Down
+
+type alias Model =
+    { board : Plane Cell
+    , snake : List (Point Cell)
+    , score : Int
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( (), Cmd.none )
+
 
 view : Model -> Html Msg
-view _ = text "Snake"
+view _ =
+    text "Snake"
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = (model, Cmd.none)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
